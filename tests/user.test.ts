c4153977fccb0e6be2e77930a7f21b930961cba0
Guilherme Alves Pimenta <guialvespimenta27@gmail.com>
2023-01-13 09:11:29 -0300

    style: calling usecases as suts in tests

--- tests/user.test.ts
+++ tests/user.test.ts
@@ -4,10 +4,10 @@ import { UserBuilder } from "./utils/builder/user-builder";
 
 it("should be able to create a new user on database", async () => {
   const userRepository = new InMemoryUserRepository();
-  const usecase = new CreateUser(userRepository);
-  const defaultUser = UserBuilder.aUser().build();
 
-  await usecase.execute(defaultUser);
+  const sut = new CreateUser(userRepository);
+  const defaultUser = UserBuilder.aUser().build();
+  await sut.execute(defaultUser);
 
   const user = await userRepository.findByCPF("12345678910");
   expect(user).toHaveProperty("id");
@@ -23,9 +23,9 @@ it("should not be able to create a new user with an existing CPF", async () => {
   const user = UserBuilder.aUser().withCPF("12345678910").build();
   await userRepository.create(user);
 
-  const usecase = new CreateUser(userRepository);
+  const sut = new CreateUser(userRepository);
   const userWithExistentCPF = UserBuilder.aUser().withCPF("12345678910").build();
-  await expect(usecase.execute(userWithExistentCPF)).rejects.toThrow("CPF already exists");
+  await expect(sut.execute(userWithExistentCPF)).rejects.toThrow("CPF already exists");
 });
 
 it("should not be able to create a new user with an existing email", async () => {
@@ -33,7 +33,7 @@ it("should not be able to create a new user with an existing email", async () =>
   const user = UserBuilder.aUser().withEmail("john_doe@gmail.com").build();
   await userRepository.create(user);
 
-  const usecase = new CreateUser(userRepository);
+  const sut = new CreateUser(userRepository);
   const userWithExistentEmail = UserBuilder.aUser().withCPF("01234567891").withEmail("john_doe@gmail.com").build();
-  await expect(usecase.execute(userWithExistentEmail)).rejects.toThrow("Email already exists");
+  await expect(sut.execute(userWithExistentEmail)).rejects.toThrow("Email already exists");
 });
